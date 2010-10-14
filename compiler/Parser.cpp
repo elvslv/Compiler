4001a998afc1cefd60cbeeab44d776025b6c5fbd
Lena <e_vslv@mail.ru>
2010-10-14 22:35:00 +1100

    Заполнение таблицы символов

--- compiler/Parser.cpp
+++ compiler/Parser.cpp
@@ -1,11 +1,294 @@
 #include "Parser.h"
+static int id = 0;
 map<string, int> priority;
 const int arrSize = 500;
 char arr[arrSize][arrSize];
 static int maxLength = 0;
 
-int PaintBranch(int i, int j, int k, int h, bool f) 
-{
+void Parser::ParseDecl(){
+	while (TokVal() == "TYPE" || TokVal() == "VAR" || TokVal() == "CONST" || TokVal() == "PROCEDURE" || 
+		TokVal() == "FUNCTION"){
+		string s = TokVal();
+		scan.Next();
+		if (s == "TYPE")
+			ParseTypeBlock();
+		else if (s == "VAR")
+			ParseVarBlock();
+		else if (s == "CONST")
+			ParseConstBlock();
+		else if (s == "FUNCTION")
+			ParseFunction(true);
+		else if (s == "PROCEDURE")
+			ParseProcedure(true);
+	}
+	/*if (TokVal() == "BEGIN"){
+		ParseAssignment();
+	}*/
+}
+
+string Parser::CheckCurTok(string blockName, SymTable* tbl){
+	if (TokType() != ttIdentifier)
+		throw Error("Invalid lexem in " + blockName + " block", TokPos(), TokLine());
+	string s = TokVal();
+	transform(s.begin(), s.end(), s.begin(), toupper);
+	SymTable::iterator it = tbl->find(s);
+	if (it != tbl->end())
+		throw Error("Identifier already declarated", TokPos(), TokLine());	
+	scan.Next();
+	return s;
+}
+
+SymTable* Parser::GetArgs(){
+	list<string> args;
+	SymTable* argsTable = NULL;
+	SymType* type = NULL;
+	if (TokVal() == "("){
+		while (TokVal() != ")"){
+			scan.Next();
+			string s = TokVal();
+			if (s == "VAR")
+				scan.Next();
+			while (TokVal() != ";" && TokVal() != ")"){
+				scan.Next();
+				string par = CheckCurTok("function", argsTable);
+				args.push_back(par);
+				if (TokVal() == ",")
+					scan.Next();
+				else if (TokVal() == ":"){
+					scan.Next();
+					type = ParseType(false);
+					scan.Next();
+					break;
+				}
+				else if (TokVal() != ")" && TokVal() != ";")
+					throw Error("Invalid lexem in function declaration", TokPos(), TokLine());
+			}
+			for (list<string>::iterator it = args.begin(); it != args.end(); ++it){
+				if (s == "VAR")
+					(*argsTable)[*it] = new SymVarParamByRef(type);
+				else
+					(*argsTable)[*it] = new SymVarParamByValue(type);
+			}
+			args.empty();			
+			if (TokVal() != ")")
+				scan.Next();
+		}
+	}
+	return argsTable;
+}
+
+SymProc* Parser::ParseProcedure(bool newProc){
+	string ident;
+	if (newProc)
+		string ident = CheckCurTok("procedure", table);
+	SymTable* argsTable = GetArgs();
+	scan.Next();
+	SymProc* res = new SymProc(argsTable, NULL);
+	if (TokVal() != ";")
+		throw Error("; expected", TokPos(), TokLine());
+	scan.Next();
+	if (TokVal() != "FORWARD")
+		throw Error("Forward expected", TokPos(), TokLine());
+	scan.Next();
+	if (TokVal() != ";")
+		throw Error("; expected", TokPos(), TokLine());
+	if (newProc)
+		(*table)[ident] = res;
+	return res;
+}
+
+SymFunc* Parser::ParseFunction(bool newFunc){
+	string ident;
+	if (newFunc)
+		string ident = CheckCurTok("function", table);
+	SymType* type = NULL;
+	SymTable* argsTable = GetArgs();
+	scan.Next();
+	if (TokVal() != ":")
+		throw Error("Function type expected", TokPos(), TokLine());
+	scan.Next();
+	type = ParseType(false);
+	SymFunc* res = new SymFunc(argsTable, NULL, type);
+	scan.Next();
+	if (TokVal() != ";")
+		throw Error("; expected", TokPos(), TokLine());
+	scan.Next();
+	if (TokVal() != "FORWARD")
+		throw Error("Forward expected", TokPos(), TokLine());
+	scan.Next();
+	if (TokVal() != ";")
+		throw Error("; expected", TokPos(), TokLine());
+	if (newFunc)
+		(*table)[ident] = res;
+	return res;
+}
+
+void Parser::ParseVarBlock(){
+	list<string> idents;
+	while(true){
+		string s = CheckCurTok("var", table);
+		idents.push_back(s);
+		if (TokVal() == ",")
+			scan.Next();
+		else if (TokVal() == ":"){
+			scan.Next();
+			SymType* type = ParseType(false);
+			for (list<string>::iterator it1 = idents.begin(); it1 != idents.end(); ++it1)
+				(*table)[*it1] = type;
+			scan.Next();
+			if (TokVal() != ";")
+				throw Error("; Expected", TokPos(), TokLine());
+			scan.Next();
+			if (TokVal() == "TYPE" || TokVal() == "CONST" || TokVal() == "PROCEDURE" || 
+				TokVal() == "FUNCTION")
+				break;
+			if (TokVal() == "VAR")
+				scan.Next();
+		}
+	}
+}
+
+SymTable* Parser::ParseRecordBlock(){
+	SymTable* recTable;
+	list<string> idents;
+	while(TokVal() != "END"){
+		string s = CheckCurTok("record", recTable);
+		idents.push_back(s);
+		if (TokVal() == ",")
+			scan.Next();
+		else if (TokVal() == ":"){
+			scan.Next();
+			SymType* type = ParseType(false);
+			for (list<string>::iterator it1 = idents.begin(); it1 != idents.end(); ++it1)
+				(*recTable)[*it1] = type;
+			scan.Next();
+			if (TokVal() != ";")
+				throw Error("; Expected", TokPos(), TokLine());
+			scan.Next();
+		} 
+		else 
+			throw Error("Invalid lexem in record block", TokPos(), TokLine());
+	}
+	scan.Next();
+	return recTable;
+}
+
+SymVarConst* Parser::ParseConst(){
+	SymVarConst* res = NULL;
+	Expr* expr = ParseSimple(4);
+	SymType* type = NULL;
+	if (!IsConst(expr->ExprType()))
+		throw Error("Invalid const declaration", TokPos(), TokLine());
+	if (expr->ExprType() == ttIntLit)
+		type = new SymTypeInteger();
+	else if (expr->ExprType() == ttRealLit)
+		type = new SymTypeFloat();
+	res = new SymVarConst(type);
+	return res;
+}
+
+void Parser::ParseConstBlock(){
+	while (true){
+		string ident = CheckCurTok("constants", table);
+		if (TokVal() != "=")
+			throw Error("Invalid lexem in const declaration", TokPos(), TokLine());
+		scan.Next();
+		(*table)[ident] = ParseConst();
+		scan.Next();
+		if (TokVal() != ";")
+			throw Error("; Expected", TokPos(), TokLine());
+		scan.Next();
+		if (TokVal() == "TYPE" || TokVal() == "VAR" || TokVal() == "PROCEDURE" || 
+			TokVal() == "FUNCTION")
+			break;
+		if (TokVal() == "CONST")
+			scan.Next();
+	}
+}
+
+SymType* Parser::ParseType(bool newType){
+	SymType* res = NULL;
+	string s = TokVal();
+	transform(s.begin(), s.end(), s.begin(), toupper);
+	SymTable::iterator it = table->find(s);
+	if (it != table->end()){
+		if (newType){
+			if (!((it->second)->IsType()))
+				throw Error("Invalid lexem in type declaration", TokPos(), TokLine());
+			res = new SymTypeAlias((SymType*)it->second);	
+		} 
+		else
+			res = (SymType*)it->second;
+	} 
+	else if (s == "RECORD"){
+		SymTable* fields;
+		scan.Next();
+		fields = ParseRecordBlock();
+		res = new SymTypeRecord(fields);
+	}
+	else if (s == "ARRAY"){
+		scan.Next();
+		res = ParseArray();
+	/*} else if (s == "FUNCTION"){
+		scan.Next();
+		res = ParseFunction(false);
+	}else if (s == "PROCEDURE"){
+		scan.Next();
+		res = ParseProcedure(false);*/
+	} else if (it == table->end() && !newType)
+		throw Error("Unknown type", TokPos(), TokLine());
+	return res;
+}
+
+void Parser::ParseTypeBlock(){
+	while(true){
+		string ident = CheckCurTok("type", table);
+		if (TokVal() != "=")
+			throw Error("Invalid lexem in type declaration", TokPos(), TokLine());
+		scan.Next();
+		(*table)[ident] = ParseType(true);
+		scan.Next();
+		if (TokVal() != ";")
+			throw Error("; Expected", TokPos(), TokLine());
+		scan.Next();
+		if (TokVal() == "TYPE" || TokVal() == "VAR" || TokVal() == "PROCEDURE" || 
+			TokVal() == "FUNCTION")
+			return;
+		if (TokVal() == "CONST")
+			scan.Next();
+	}
+}
+
+SymTypeArray* Parser::ParseArray(){
+	SymTypeArray* res = NULL;
+	if (TokVal() != "[")
+		throw Error("Invalid array declaration", TokPos(), TokLine());
+	list<SymVarConst*> left, right;
+	while (TokVal() != "]"){
+		SymVarConst* l = ParseConst();
+		scan.Next();
+		if (TokVal() != "..")
+			throw Error("Invalid array declaration", TokPos(), TokLine());
+		scan.Next();
+		SymVarConst* r = ParseConst();
+		left.push_back(l);
+		right.push_back(r);
+		scan.Next();
+		if (TokVal() == ",")
+			scan.Next();
+	}
+	SymType* type = ParseType(false);
+	list<SymVarConst*>::iterator itl = left.begin();
+	list<SymVarConst*>::iterator itr = right.begin();
+	for (; itl != left.end() && itr != right.end(); ++itl, ++itr)
+	{
+		res = new SymTypeArray(type, *itl, *itr);
+		type = res;
+	}
+	return res;
+}
+
+int PaintBranch(int i, int j, int k, int h, bool f){
 	for (; k < h; ++k)
 		arr[++j][i] = '|';
 	arr[j][i] = '|';
@@ -15,8 +298,7 @@ int PaintBranch(int i, int j, int k, int h, bool f)
 	return j;
 }
 
-int FillTreeIdentConst(int i, int j, string val)
-{
+int FillTreeIdentConst(int i, int j, string val){
 	unsigned int k = 0;
 	for (; k < val.length(); ++k)
 		arr[j][i + k] = val[k];
@@ -25,8 +307,7 @@ int FillTreeIdentConst(int i, int j, string val)
 	return 2;
 }
 
-int FillTreeOp(int i, int j, string val)
-{
+int FillTreeOp(int i, int j, string val){
 	unsigned int k = 0;
 	for (; k < val.length(); ++k)
 		arr[j][i + k] = val[k];
@@ -34,8 +315,7 @@ int FillTreeOp(int i, int j, string val)
 	return j;
 }
 
-int FillTreeBinOp(int i, int j, string Value, Expr* left, Expr* right)
-{
+int FillTreeBinOp(int i, int j, string Value, Expr* left, Expr* right){
 	j = FillTreeOp(i, j, Value);
 	int hl = left->FillTree(i + 4, j);
 	j = PaintBranch(i, j, 0, hl, true);
@@ -43,33 +323,27 @@ int FillTreeBinOp(int i, int j, string Value, Expr* left, Expr* right)
 	return hl + hr + 2;
 }
 
-int Ident::FillTree(int i, int j)
-{
+int SimpleIdent::FillTree(int i, int j){
 	return FillTreeIdentConst(i, j, Value);
 }
 
-int Const::FillTree(int i, int j)
-{
+int SimpleConst::FillTree(int i, int j){
 	return FillTreeIdentConst(i, j, Value);
 }
 
-int BinaryOp::FillTree(int i, int j)
-{
+int SimpleBinaryOp::FillTree(int i, int j){
 	return FillTreeBinOp(i, j, Value, left, right);
 }
 
-int ArrayAccess::FillTree(int i, int j)
-{
+int SimpleArrayAccess::FillTree(int i, int j){
 	return FillTreeBinOp(i, j, Value, left, right);
 }
 
-int RecordAccess::FillTree(int i, int j)
-{
+int SimpleRecordAccess::FillTree(int i, int j){
 	return FillTreeBinOp(i, j, Value, left, right);
 }
 
-int FunctionCall::FillTree(int i, int j)
-{
+int SimpleFunctionCall::FillTree(int i, int j){
 	unsigned int k = 0;
 	int tmp = j;
 	j = FillTreeOp(i, j, "()");
@@ -78,8 +352,7 @@ int FunctionCall::FillTree(int i, int j)
 	j = PaintBranch(i, j, j, j + Value->FillTree(i + 4, j) - 2, false);
 	int s = 0;
 	int h = 0;
-	for (list<Expr*>::iterator it = args.begin() ;it != args.end(); ++it)
-	{
+	for (list<Expr*>::iterator it = args.begin() ;it != args.end(); ++it){
 		h = (*it)->FillTree(i + 4, j + 2);
 		j = PaintBranch(i, j, 0, h, true);
 		s += h;
@@ -87,26 +360,22 @@ int FunctionCall::FillTree(int i, int j)
 	return j - tmp + 2;
 }
 
-int UnaryOp::FillTree(int i, int j)
-{
+int SimpleUnaryOp::FillTree(int i, int j){
 	j = FillTreeOp(i, j, Value);
 	int h = child->FillTree(i + 4, j);
 	return h + 2;
 }
 
-void Expr::Print(ostream& os, int n)
-{
+void Expr::Print(ostream& os, int n){
 	int h = FillTree(0, 0) - 1;
-	for (int i = 0; i < h; ++i)
-	{
+	for (int i = 0; i < h; ++i){
 		for (int j = 0; j < maxLength; ++j)
 			os << arr[i][j];
 		os << "\n";
 	}
 }
 
-void Parser::FillMaps()
-{
+void Parser::FillMaps(){
 	priority["NOT"] = 1; 
 	priority["*"] = 2; priority["/"] = 2; priority["DIV"] = 2; priority["MOD"] = 2; 
 	priority["AND"] = 2; priority["SHL"] = 2; priority["SHR"] = 2; 
@@ -116,45 +385,39 @@ void Parser::FillMaps()
 	memset(arr, ' ', arrSize * arrSize);
 }
 
-int FindOpPrior(string str)
-{
+int FindOpPrior(string str){
 	map<string, int>::iterator it;
 	transform(str.begin(), str.end(), str.begin(), toupper);
 	it = priority.find(str);
 	return (it != priority.end()) ? it->second : 0;
 }
 
-Expr* Parser::ParseSimple(int prior)
-{
+Expr* Parser::ParseSimple(int prior){
 	if (prior == 1)
 		return ParseFactor();
 	Expr* res = ParseSimple(prior - 1);
-	while (FindOpPrior(TokVal()) == prior)
-	{
+	while (FindOpPrior(TokVal()) == prior){
 		string str = TokVal();
 		scan.Next();
 		Expr* expr1 = ParseSimple(prior - 1);
 		if (!expr1)
 			throw Error("Lexem expected, EOF found", TokPos(), TokLine());
-		res = new BinaryOp(str, res, expr1);
+		res = new SimpleBinaryOp(str, res, expr1);
 	}
 	return res;
 }
 
-bool IsUnaryOp(string str)
-{
+bool IsUnaryOp(string str){
 	transform(str.begin(), str.end(), str.begin(), toupper);
 	return str == "-" || str == "+" || str == "NOT";
 }
 
-bool IsLiteral(TokenType tt)
-{
+bool IsLiteral(TokenType tt){
 	return tt == ttIntLit || tt == ttRealLit || tt == ttStringLit || tt == ttHexLiteral || tt == ttBinLiteral 
 		|| tt == ttOctLiteral;
 }
 
-Expr* Parser::ParseNext()
-{
+Expr* Parser::ParseNext(){
 	scan.Next();
 	Expr* expr1 = ParseSimple(4);
 	if (!expr1)
@@ -162,14 +425,11 @@ Expr* Parser::ParseNext()
 	return expr1;
 }
 
-Expr* Parser::ParseFunctionCall(Expr* res, int pos, int line)
-{
+Expr* Parser::ParseFunctionCall(Expr* res, int pos, int line){
 	Expr* expr1 = ParseNext();
 	list<Expr*> arg;
-	while (TokVal() != ")")
-	{
-		if (TokVal() == ",")
-		{
+	while (TokVal() != ")"){
+		if (TokVal() == ","){
 			arg.push_back(expr1);
 			expr1 = ParseNext();
 		}
@@ -177,21 +437,18 @@ Expr* Parser::ParseFunctionCall(Expr* res, int pos, int line)
 			throw Error("Unexpected lexem found", TokPos(), TokLine());
 	}
 	arg.push_back(expr1);
-	res = new FunctionCall(res, arg);
+	res = new SimpleFunctionCall(res, arg);
 	scan.Next();
 	return res;
 }
 
-Expr* Parser::ParseArrayAccess(Expr* res, int pos, int line)
-{
+Expr* Parser::ParseArrayAccess(Expr* res, int pos, int line){
 	Expr* expr1 = ParseNext();
-	res = new ArrayAccess("[]", res, expr1);
-	while (TokVal() !=  "]")
-	{
-		if (TokVal() == ",")
-		{	
+	res = new SimpleArrayAccess("[]", res, expr1);
+	while (TokVal() !=  "]"){
+		if (TokVal() == ","){	
 			expr1 = ParseNext();
-			res = new ArrayAccess("[]", res, expr1);
+			res = new SimpleArrayAccess("[]", res, expr1);
 		}
 		else if (TokVal() != "]")
 			throw Error("Unexpected lexem found", TokPos(), TokLine());		
@@ -200,34 +457,29 @@ Expr* Parser::ParseArrayAccess(Expr* res, int pos, int line)
 	return res;
 }
 
-Expr* Parser::ParseRecordAccess(Expr* res, int pos, int line)
-{
+Expr* Parser::ParseRecordAccess(Expr* res, int pos, int line){
 	Expr* expr1 = ParseNext();
-	if (!expr1->IsIdent())
+	if (!expr1->LValue() && !expr1->IsFunction())
 		throw Error("Invalid record access", pos, line);
-	res = new RecordAccess(".", res, expr1);
-	while (TokVal() ==  ".")
-	{
+	res = new SimpleRecordAccess(".", res, expr1);
+	while (TokVal() ==  "."){
 		expr1 = ParseNext();
-		if (!expr1->IsIdent())
+		if (!expr1->LValue() && !expr1->IsFunction())
 			throw Error("Invalid record access", pos, line);
-		res = new RecordAccess(".", res, expr1);
+		res = new SimpleRecordAccess(".", res, expr1);
 	}
 	return res;
 }
 
-Expr* Parser::CallAccess(Expr* id, int pos, int line)
-{
+Expr* Parser::CallAccess(Expr* id, int pos, int line){
 	Expr* res = id;
 	isAccess = true;
-	while (TokVal() == "(" || TokVal() == "[" || TokVal() == ".")
-	{
+	while (TokVal() == "(" || TokVal() == "[" || TokVal() == "."){
 		if (TokVal() == "(")
 			res = ParseFunctionCall(res, pos, line);
 		else if (TokVal() == "[")
 			res = ParseArrayAccess(res, pos, line);
-		else if (TokVal() == ".")
-		{
+		else if (TokVal() == "."){
 			isRecord = true;
 			res = ParseRecordAccess(res, pos, line);
 		}
@@ -237,8 +489,7 @@ Expr* Parser::CallAccess(Expr* id, int pos, int line)
 	return res;
 }
 
-Expr* Parser::ParseFactor()
-{
+Expr* Parser::ParseFactor(){
 	Expr* res = NULL;
 	int pos = TokPos();
 	int line = TokLine();
@@ -246,18 +497,16 @@ Expr* Parser::ParseFactor()
 		return NULL;
 	if (TokType() == ttBadToken)
 		throw Error("Invalid lexem", pos, line);
-	if (IsUnaryOp(TokVal()))
-	{
+	if (IsUnaryOp(TokVal())){
 		string str = TokVal();
 		scan.Next();
 		Expr* expr1 = ParseFactor();
 		if (!expr1)
 			throw Error("Lexem expected, EOF found", TokPos(), TokLine());
-		res = new UnaryOp(str, expr1);
+		res = new SimpleUnaryOp(str, expr1);
 	}
-	else if (TokType() == ttIdentifier)
-	{
-		Ident* id = new Ident(TokVal());
+	else if (TokType() == ttIdentifier){
+		SimpleIdent* id = new SimpleIdent(TokVal());
 		scan.Next();
 		if (TokVal() == "[" || TokVal() == "(" || (TokVal() == "." && !isRecord))
 			res = CallAccess(id, pos, line);
@@ -265,13 +514,11 @@ Expr* Parser::ParseFactor()
 			res = id;
 		
 	}
-	else if (IsLiteral(TokType()))
-	{
-		res = new Const(TokVal());
+	else if (IsLiteral(TokType())){
+		res = new SimpleConst(TokVal());
 		scan.Next();
 	}
-	else if (TokVal() == "(")
-	{
+	else if (TokVal() == "("){
 		scan.Next();
 		if (TokVal() == ")")
 			throw Error("Empty bracket sequence", pos, line);
@@ -279,7 +526,7 @@ Expr* Parser::ParseFactor()
 		if (TokVal() != ")")
 			throw Error("Unclosed bracket", pos, line);
 		scan.Next();
-		if (res->IsIdent() && !isAccess)
+		if (res->IsFunction() || res->LValue() && !isAccess)
 			res = CallAccess(res, pos, line);
 	}
 	else
--- compiler/Parser.h
+++ compiler/Parser.h
@@ -7,85 +7,262 @@
 #include "Scanner.h"
 using namespace std;
 
+class Symbol{
+public:
+	virtual bool IsType() {return false;}
+private:
+	string name;
+};
+
+bool IsConst(TokenType type){
+	return type == ttIntLit || type == ttRealLit;
+}
+
+typedef map <string, Symbol*> SymTable;
+
+class SymType: public Symbol{
+public:
+	virtual bool IsType() {return true;}
+public:
+	SymType(){};
+};
+
+class SymVar: public Symbol{
+public:
+	SymVar(SymType* t): type(t){};
+private:
+	SymType* type;
+};
+
+class SymProc: public Symbol{
+public:
+	SymProc(SymTable* ar, SymTable* loc): args(ar), locals(loc) {};
+private:
+	SymTable* args;
+	SymTable* locals; 
+};
+
+class SymFunc: public SymProc{
+public:
+	SymFunc(SymTable* ar, SymTable* loc, SymType* t): SymProc(ar, loc), type(t) {};
+private:
+	SymTable* args;
+	SymTable* locals; 
+	SymType* type;
+};
+
+class SymTypeScalar: public SymType{
+};
+
+class SymTypeFloat: public SymTypeScalar{
+};
+
+class SymTypeInteger: public SymTypeScalar{
+};
+
+class SymTypeRecord: public SymType{
+public:
+	SymTypeRecord(SymTable* f): fields(f) {};
+private:
+	SymTable* fields;
+};
+
+class SymTypeAlias: public SymType{
+public:
+	SymTypeAlias(SymType* rt): refType(rt){};
+private:
+	SymType* refType;
+};
+
+class SymVarLocal: public SymVar{
+};
+
+class SymVarGlobal: public SymVar{
+};
+
+class SymVarConst: public SymVar{
+public:
+	SymVarConst(SymType* t): SymVar(t){};
+};
+
+class SymVarParam: public SymVar{
+public:
+	SymVarParam(SymType* t): SymVar(t){};
+};
+
+class SymVarParamByValue: public SymVarParam{
+public:
+	SymVarParamByValue(SymType* t): SymVarParam(t){};
+};
+
+class SymVarParamByRef: public SymVarParam{
+public:
+	SymVarParamByRef(SymType* t): SymVarParam(t){};
+};
+
+class SymTypeArray: public SymType{
+public:
+	SymTypeArray(SymType* type, SymVarConst* b, SymVarConst* t): elemType(type), bottom(b), top(t) {};
+private:
+	SymType* elemType;
+	SymVarConst* bottom; SymVarConst* top;
+};
+
+class SyntaxNode{
+};
+
+class NodeStatement: public SyntaxNode{
+};
+
+class StatementAssign: public NodeStatement{
+};
+
 int FindOpPrior(string str);
 
-class Expr
-{
+class Expr{
 public:
 	Expr(){ Value = ""; }
 	void Print(ostream& os, int n);
 	virtual int FillTree(int i, int j) = 0;
 	string GetValue() { return Value; }
-	virtual bool IsIdent() { return false; }
+	virtual bool LValue() { return false; }
+	virtual bool IsFunction() {return false; }
+	virtual TokenType ExprType() {return ttToken;}
 protected:
 	string Value;
 };
 
-class Ident: public Expr
-{
+class Ident: public Expr{
 public:
-	Ident(string val){ Value = val; }
+	Ident(SymVar* symb): symbol(symb) {};
 	int FillTree(int i, int j);
-	bool IsIdent() { return true; }
+private:
+	SymVar* symbol;
 };
 
-class Const: public Expr
-{
+class Const: public Expr{
 public:
-	Const(string val){ Value = val; }
+	Const(SymVar* symb): symbol(symb){};
 	int FillTree(int i, int j);
+	TokenType ExprType() {return type;}
+private:
+	TokenType type;
+	SymVar* symbol;
 };
 
-class BinaryOp: public Expr
-{
+class BinaryOp: public Expr{
 public: 
 	BinaryOp(string val, Expr* l, Expr* r) : left(l), right(r) { Value = val; }
+	int FillTree(int i, int j);
+	TokenType ExprType() {
+		TokenType lt = left->ExprType();
+		TokenType rt = right->ExprType();
+		return lt == rt && IsConst(lt) ? lt : ttOperation;
+	}
+protected:
+	Expr* right;
+	Expr* left;
+};
+
+class ArrayAccess: public BinaryOp{
+public: 
+	ArrayAccess(string val, Expr* l, Expr* r): BinaryOp(val, l, r){};
+	int FillTree(int i, int j);
+	bool IsIdent() { return true; }
+	TokenType ExprType() {return ttOperation;}
+};
+
+class RecordAccess: public Expr{
+public: 
+	RecordAccess(string val, Expr* l, SymVar* r): left(l), right(r) {	Value = val; }
+	int FillTree(int i, int j);
+	TokenType ExprType() {return ttOperation;}
+private:
+	SymVar* right;
+	Expr* left;
+};
+
+class FunctionCall: public Expr{
+public: 
+	FunctionCall(SymFunc* val, list<Expr*> ar){
+		Value = val;
+		args = ar;
+	}
+	int FillTree(int i, int j);
+	TokenType ExprType() {return ttOperation;}
+private:
+	list<Expr*> args;
+	SymFunc* Value;
+};
+
+
+class UnaryOp: public Expr{
+public: 
+	UnaryOp(string val, Expr* ch): child(ch){ 
+		Value = val;
+	}
+	int FillTree(int i, int j);
+	TokenType ExprType() {return child->ExprType();}
+private:
+	Expr* child;
+};
+
+class SimpleIdent: public Expr{
+public:
+	SimpleIdent(string val){ Value = val; }
+	int FillTree(int i, int j);
+	bool LValue() { return true; }
+};
+
+class SimpleConst: public Expr{
+public:
+	SimpleConst(string val){ Value = val; }
+	int FillTree(int i, int j);
+};
+
+class SimpleBinaryOp: public Expr{
+public: 
+	SimpleBinaryOp(string val, Expr* l, Expr* r) : left(l), right(r) { Value = val; }
 	int FillTree(int i, int j);
 protected:
 	Expr* right;
 	Expr* left;
 };
 
-class ArrayAccess: public BinaryOp
-{
+class SimpleArrayAccess: public SimpleBinaryOp{
 public: 
-	ArrayAccess(string val, Expr* l, Expr* r): BinaryOp(val, l, r){};
+	SimpleArrayAccess(string val, Expr* l, Expr* r): SimpleBinaryOp(val, l, r){};
 	int FillTree(int i, int j);
-	bool IsIdent() { return true; }
+	bool LValue() { return true; }
 };
 
-class RecordAccess: public Expr
-{
+class SimpleRecordAccess: public Expr{
 public: 
-	RecordAccess(string val, Expr* l, Expr* r): left(l), right(r) {	Value = val; }
+	SimpleRecordAccess(string val, Expr* l, Expr* r): left(l), right(r) {Value = val;}
 	int FillTree(int i, int j);
-	bool IsIdent() { return true; }
+	bool LValue() { return true; }
 private:
 	Expr* right;
 	Expr* left;
 };
 
-class FunctionCall: public Expr
-{
+class SimpleFunctionCall: public Expr{
 public: 
-	FunctionCall(Expr* val, list<Expr*> ar)
-	{
+	SimpleFunctionCall(Expr* val, list<Expr*> ar){
 		Value = val;
 		args = ar;
 	}
 	int FillTree(int i, int j);
-	bool IsIdent() {return true; }
+	bool IsFunction() {return true; }
 private:
 	list<Expr*> args;
 	Expr* Value;
 };
 
 
-class UnaryOp: public Expr
-{
+class SimpleUnaryOp: public Expr{
 public: 
-	UnaryOp(string val, Expr* ch): child(ch) 
-	{ 
+	SimpleUnaryOp(string val, Expr* ch): child(ch){ 
 		Value = val;
 	}
 	int FillTree(int i, int j);
@@ -93,11 +270,20 @@ private:
 	Expr* child;
 };
 
-class Parser
-{
+class Parser{
 public:
-	Parser(Scanner& sc): scan(sc){ FillMaps(); scan.Next(); isRecord = false; isAccess = false;}
+	Parser(Scanner& sc): scan(sc){
+		FillMaps();
+		scan.Next();
+		isRecord = false;
+		isAccess = false;
+		SymTypeInteger* i = new SymTypeInteger();
+		SymTypeFloat* f = new SymTypeFloat();
+		(*table)["INTEGER"] = i;
+		(*table)["FLOAT"] = f;
+	}
 	Expr* ParseSimpleExpr() { return ParseSimple(4); } 
+	SymTable* GetSymTable() {return table;}
 private:
 	Expr* ParseSimple(int prior);
 	Expr* ParseFactor(); 
@@ -106,11 +292,26 @@ private:
 	Expr* ParseRecordAccess(Expr* res,  int pos, int line);
 	Expr* ParseNext();
 	Expr* CallAccess(Expr* id, int pos, int line);
+	void ParseAssignment();
+	void ParseDecl();
+	void ParseTypeBlock();
+	SymType* ParseType(bool newType);
+	void ParseVarBlock();
+	SymVar* ParseVar();
+	void ParseConstBlock();
+	SymVarConst* ParseConst();
+	SymFunc* ParseFunction(bool newFunc);
+	SymProc* ParseProcedure(bool newProc);
+	SymTable* ParseRecordBlock();
+	SymTypeArray* ParseArray();
+	SymTable* GetArgs();
 	TokenType TokType() {return scan.GetToken()->GetType(); }
 	string TokVal() {return scan.GetToken()->GetValue(); }
 	int TokPos() {return scan.GetToken()->GetPos();}
 	int TokLine() {return scan.GetToken()->GetLine();}
+	string CheckCurTok(string blockName, SymTable* tbl);
 	Scanner& scan;
+	SymTable* table;
 	void FillMaps();
 	bool isRecord;
 	bool isAccess;
